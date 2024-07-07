df7d86c9ebdbcca1d248d1ed548539e93d7b45eb
Victor <victor.ched@gmail.com>
2024-07-07 13:07:23 -0400

    Improvement

--- source/__init__.py
+++ source/__init__.py
@@ -1,5 +1,6 @@
 import bpy
 from .panel import *
+from .get_addons import get_addons
 
 bl_info = {
     "name": "Extension Reviewer",
@@ -10,20 +11,23 @@ bl_info = {
     "location": "View3D",
 }
 
-classes = (EXTENSIONREVIEW_PT_Panel,EXTENSIONREVIEW_OT_ToggleTextbox)
+classes = (EXTENSIONREVIEW_PT_Panel, EXTENSIONREVIEW_OT_ToggleTextbox)
 
-        
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-
-
-
+    
+    addon_list = get_addons()
+    for addon in addon_list:
+        setattr(bpy.types.Scene, addon, bpy.props.BoolProperty(name=addon, default=False))
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-
+    
+    addon_list = get_addons()
+    for addon in addon_list:
+        delattr(bpy.types.Scene, addon)
 
 if __name__ == "__main__":
     register()
--- source/panel.py
+++ source/panel.py
@@ -1,31 +1,16 @@
 import bpy
-from bpy.props import BoolProperty
-from bpy.types import Operator
+from bpy.types import Operator, Panel
 from .get_addons import get_addons, get_toml_data
-
-
-def iniSceneProperties(scn):
-    bpy.types.Scene.show_textbox = BoolProperty(
-        name="Show Text Box", description="A boolean property", default=False
-    )
-    scn["show_textbox"] = False
-
 
 class EXTENSIONREVIEW_OT_ToggleTextbox(Operator):
     bl_idname = "extensionreview.toggle_textbox"
     bl_label = "Toggle Text Box"
 
     def execute(self, context):
-        scn = context.scene
-        scn["show_textbox"] = not scn.get("show_textbox", False)
-        self.report(
-            {"INFO"}, f"Text box {'shown' if scn['show_textbox'] else 'hidden'}"
-        )
+        # Placeholder for any future operation needed
+        return {'FINISHED'}
 
-        return {"FINISHED"}
-
-
-class EXTENSIONREVIEW_PT_Panel(bpy.types.Panel):
+class EXTENSIONREVIEW_PT_Panel(Panel):
     bl_label = "Get Addons List"
     bl_idname = "GETADDONSLIST_PT_panel"
     bl_space_type = "VIEW_3D"
@@ -34,15 +19,14 @@ class EXTENSIONREVIEW_PT_Panel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        scn = context.scene
-
         addon_list = get_addons()
 
         for addon_name in addon_list:
-            layout.operator("extensionreview.toggle_textbox", text=addon_name)
+            row = layout.row()
+            row.prop(context.scene, addon_name, toggle=True, text=addon_name)
 
-        if scn.get("show_textbox", False):
-            toml_content = get_toml_data(addon_name)
-
-            for content in toml_content:
-                layout.label(text=content)
+            if getattr(context.scene, addon_name):
+                toml_content = get_toml_data(addon_name)
+                for content in toml_content:
+                    row = layout.row()
+                    row.label(text=content)
