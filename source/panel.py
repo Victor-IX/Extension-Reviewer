cb645877396299853c51211decdd9025a723b373
Victor <victor.ched@gmail.com>
2024-07-16 20:39:54 -0400

    add message constructor

--- source/panel.py
+++ source/panel.py
@@ -1,6 +1,7 @@
 import bpy
 import os
 import toml
+import subprocess
 from bpy.types import Operator, Panel
 from .get_addons import get_addons, get_toml_data
 from .utils import has_lower_upper_pair, get_addon_path
@@ -21,9 +22,20 @@ class EXTENSIONREVIEW_OT_toggle_textbox(Operator):
             messages = toml.load(file)
 
         messages_list = []
+        separator = True
+        error_intro_message = False
+        warning_intro_message = False
+
+        builder_messages = {
+            "intro_message": messages["intro_message"],
+            "separator": messages["separator"],
+            "error_list": messages["error_list"],
+            "warning_list": messages["warning_list"],
+        }
 
         error_messages = {
             "blender_name": messages["blender_name"],
+            "blender_logo": messages["blender_logo"],
             "remnant_files": messages["remnant_files"],
             "missing_permission_network": messages["missing_permission_network"],
             "missing_permission_file": messages["missing_permission_file"],
@@ -37,17 +49,34 @@ class EXTENSIONREVIEW_OT_toggle_textbox(Operator):
         }
 
         if errors:
+            messages_list.append(builder_messages["intro_message"])
+            messages_list.append(builder_messages["separator"])
             for error in errors:
                 if error in error_messages:
+                    if not error_intro_message:
+                        messages_list.append(builder_messages["error_list"])
+                        error_intro_message = False
+
+                    separator = False
                     messages_list.append(error_messages[error])
                     self.report({"ERROR"}, error_messages[error])
                 elif error in warning_messages:
+                    if not separator:
+                        messages_list.append(builder_messages["separator"])
+                        separator = True
+                    if not warning_intro_message:
+                        messages_list.append(builder_messages["warning_list"])
+                        warning_intro_message = True
                     messages_list.append(warning_messages[error])
                     self.report({"WARNING"}, warning_messages[error])
         else:
             self.report({"INFO"}, "No errors found.")
 
-        print(messages_list)
+        for message in messages_list:
+            print(message)
+
+        final_message = "\n".join(messages_list)
+        subprocess.run("clip", universal_newlines=True, input=final_message)
 
         return {"FINISHED"}
 
